9821d2bff3f3dd0543b271c20ca165384f25d1ae
Jorge Josue Garcia Garcia <98290583+JorgeGa2612@users.noreply.github.com>
2025-06-03 23:25:11 -0600

    Prueba Correcciones

--- scripts.js
+++ scripts.js
@@ -2,127 +2,127 @@
 var chapters = {
     'prologo': [{ text: 'Introducción', link: './prologo/introduccion.html' }],
     'capitulo1': [
-        { text: 'Empecemos I', link: '/Capitulo 1/EmpecemosI.html' }, 
-        { text: 'Empecemos II', link: '/Capitulo 1/EmpecemosII.html' }, 
-        { text: 'Empecemos III', link: '/Capitulo 1/EmpecemosIII.html' }, 
-        { text: 'Origen de la filosofia: Maravilla', link: '/Capitulo 1/origen.html' }, 
-        { text: 'Maravilla: Caos y Cosmos', link: '/Capitulo 1/maravilla.html' }, 
-        { text: 'El Origen De La Filosofia: Los Griegos y Nosotros', link: '/Capitulo 1/griegos.html' }],
+        { text: 'Empecemos I', link: '/.Capitulo 1/EmpecemosI.html' }, 
+        { text: 'Empecemos II', link: '/.Capitulo 1/EmpecemosII.html' }, 
+        { text: 'Empecemos III', link: '/.Capitulo 1/EmpecemosIII.html' }, 
+        { text: 'Origen de la filosofia: Maravilla', link: '/.Capitulo 1/origen.html' }, 
+        { text: 'Maravilla: Caos y Cosmos', link: '/.Capitulo 1/maravilla.html' }, 
+        { text: 'El Origen De La Filosofia: Los Griegos y Nosotros', link: '/.Capitulo 1/griegos.html' }],
     'capitulo2': [
-        { text: 'Pensamiento Griego: Visión General', link: '/Capitulo 2/PensamientoGriegoVisionGeneral.html' },
-        { text: 'Cultura Griega', link: '/Capitulo 2/CulturaGriega.html' },
-        { text: 'Teatro Griego', link: '/Capitulo 2/TeatroGriego.html' },
-        { text: 'Los Sofistas', link: '/Capitulo 2/LosSofistas.html' },
-        { text: 'Pre-Socráticos', link: '/Capitulo 2/PreSocraticos.html' },
-        { text: 'La Vida de Sócrates', link: '/Capitulo 2/LaVidadeSocrates.html' },
-        { text: 'El Juicio de Sócrates', link: '/Capitulo 2/ElJuiciodeSocrates.html' },
-        { text: 'Sócrates en Prisión', link: '/Capitulo 2/SocratesenPrision.html' },
-        { text: 'La Muerte de Sócrates: La Teoría de las Formas I', link: '/Capitulo 2/LaMuertedeSocratesI.html' },
-        { text: 'La Muerte y Legado de Sócrates II', link: '/Capitulo 2/LaMuerteyLegadodeSocratesII.html' },
-        { text: 'La Muerte y Legado de Sócrates III', link: '/Capitulo 2/LaMuerteyLegadodeSocratesIII.html' },
-        { text: 'El Legado de Sócrates IV', link: '/Capitulo 2/ElLegadodeSocratesIV.html' },
-        { text: 'Diálogos de Platón', link: '/Capitulo 2/DialogosdePlaton.html' },
-        { text: 'Platón: Visión General', link: '/Capitulo 2/PlatonVisionGeneral.html' },
-        { text: 'Platón: La República', link: '/Capitulo 2/PlatonLaRepublica.html' },
-        { text: 'Platón: El Simposio, El Proceso', link: '/Capitulo 2/PlatonElSimposioElProceso.html' },
-        { text: 'Legado de Platón', link: '/Capitulo 2/LegadodePlaton.html' }
+        { text: 'Pensamiento Griego: Visión General', link: '/.Capitulo 2/PensamientoGriegoVisionGeneral.html' },
+        { text: 'Cultura Griega', link: '/.Capitulo 2/CulturaGriega.html' },
+        { text: 'Teatro Griego', link: '/.Capitulo 2/TeatroGriego.html' },
+        { text: 'Los Sofistas', link: '/.Capitulo 2/LosSofistas.html' },
+        { text: 'Pre-Socráticos', link: '/.Capitulo 2/PreSocraticos.html' },
+        { text: 'La Vida de Sócrates', link: '/.Capitulo 2/LaVidadeSocrates.html' },
+        { text: 'El Juicio de Sócrates', link: '/.Capitulo 2/ElJuiciodeSocrates.html' },
+        { text: 'Sócrates en Prisión', link: '/.Capitulo 2/SocratesenPrision.html' },
+        { text: 'La Muerte de Sócrates: La Teoría de las Formas I', link: '/.Capitulo 2/LaMuertedeSocratesI.html' },
+        { text: 'La Muerte y Legado de Sócrates II', link: '/.Capitulo 2/LaMuerteyLegadodeSocratesII.html' },
+        { text: 'La Muerte y Legado de Sócrates III', link: '/.Capitulo 2/LaMuerteyLegadodeSocratesIII.html' },
+        { text: 'El Legado de Sócrates IV', link: '/.Capitulo 2/ElLegadodeSocratesIV.html' },
+        { text: 'Diálogos de Platón', link: '/.Capitulo 2/DialogosdePlaton.html' },
+        { text: 'Platón: Visión General', link: '/.Capitulo 2/PlatonVisionGeneral.html' },
+        { text: 'Platón: La República', link: '/.Capitulo 2/PlatonLaRepublica.html' },
+        { text: 'Platón: El Simposio, El Proceso', link: '/.Capitulo 2/PlatonElSimposioElProceso.html' },
+        { text: 'Legado de Platón', link: '/.Capitulo 2/LegadodePlaton.html' }
       ],
       'capitulo3': [
-        { text: 'Visión General', link: '/Capitulo 3/VisionGeneral.html' },
-        { text: 'Pruebas de la Existencia de Dios: Visión General', link: '/Capitulo 3/PruebasExistenciaDios.html' },
-        { text: 'Argumento de la Revelación', link: '/Capitulo 3/ArgumentoRevelacion.html' },
-        { text: 'Argumento de la Razón: Argumento Ontológico', link: '/Capitulo 3/ArgumentoOntologico.html' },
-        { text: 'Argumento de la Razón: Argumento Cosmológico', link: '/Capitulo 3/ArgumentoCosmologico.html' },
-        { text: 'Argumento de la Razón: Argumento Teleológico', link: '/Capitulo 3/ArgumentoTeleologico.html' },
-        { text: 'Argumento de la Experiencia: Directo', link: '/Capitulo 3/ArgumentoExperienciaDirecto.html' },
-        { text: 'Argumento de la Experiencia: Milagros', link: '/Capitulo 3/ArgumentoExperienciaMilagros.html' },
-        { text: 'Argumento de la Experiencia: Fenómenos Psíquicos', link: '/Capitulo 3/ArgumentoExperienciaPsiquicos.html' },
-        { text: 'Argumento: Pragmatismo', link: '/Capitulo 3/ArgumentoPragmatismo.html' },
-        { text: 'El Problema del Mal', link: '/Capitulo 3/ProblemaDelMal.html' },
-        { text: 'Resumen de Argumentos', link: '/Capitulo 3/ResumenArgumentos.html' },
-        { text: 'Lenguaje Religioso y Visiones del Mundo', link: '/Capitulo 3/LenguajeReligioso.html' },
-        { text: 'Opcional: ¿Qué es la Religión?', link: '/Capitulo 3/QueEsLaReligion.html' },
-        { text: 'Conclusión', link: '/Capitulo 3/Conclusion.html' }
+        { text: 'Visión General', link: '/.Capitulo 3/VisionGeneral.html' },
+        { text: 'Pruebas de la Existencia de Dios: Visión General', link: '/.Capitulo 3/PruebasExistenciaDios.html' },
+        { text: 'Argumento de la Revelación', link: '/.Capitulo 3/ArgumentoRevelacion.html' },
+        { text: 'Argumento de la Razón: Argumento Ontológico', link: '/.Capitulo 3/ArgumentoOntologico.html' },
+        { text: 'Argumento de la Razón: Argumento Cosmológico', link: '/.Capitulo 3/ArgumentoCosmologico.html' },
+        { text: 'Argumento de la Razón: Argumento Teleológico', link: '/.Capitulo 3/ArgumentoTeleologico.html' },
+        { text: 'Argumento de la Experiencia: Directo', link: '/.Capitulo 3/ArgumentoExperienciaDirecto.html' },
+        { text: 'Argumento de la Experiencia: Milagros', link: '/.Capitulo 3/ArgumentoExperienciaMilagros.html' },
+        { text: 'Argumento de la Experiencia: Fenómenos Psíquicos', link: '/.Capitulo 3/ArgumentoExperienciaPsiquicos.html' },
+        { text: 'Argumento: Pragmatismo', link: '/.Capitulo 3/ArgumentoPragmatismo.html' },
+        { text: 'El Problema del Mal', link: '/.Capitulo 3/ProblemaDelMal.html' },
+        { text: 'Resumen de Argumentos', link: '/.Capitulo 3/ResumenArgumentos.html' },
+        { text: 'Lenguaje Religioso y Visiones del Mundo', link: '/.Capitulo 3/LenguajeReligioso.html' },
+        { text: 'Opcional: ¿Qué es la Religión?', link: '/.Capitulo 3/QueEsLaReligion.html' },
+        { text: 'Conclusión', link: '/.Capitulo 3/Conclusion.html' }
       ],
       'capitulo4': [
-        { text: 'Visión General', link: '/Capitulo 4/VisionGeneral.html' },
-        { text: 'Materialismo', link: '/Capitulo 4/Materialismo.html' },
-        { text: 'Idealismo', link: '/Capitulo 4/Idealismo.html' },
-        { text: 'Dualismo', link: '/Capitulo 4/Dualismo.html' },
-        { text: 'Existencialismo', link: '/Capitulo 4/Existencialismo.html' },
-        { text: 'Pragmatismo', link: '/Capitulo 4/Pragmatismo.html' },
-        { text: 'Postmodernismo', link: '/Capitulo 4/Postmodernismo.html' },
-        { text: 'Conclusión', link: '/Capitulo 4/Conclusion.html' }
+        { text: 'Visión General', link: '/.Capitulo 4/VisionGeneral.html' },
+        { text: 'Materialismo', link: '/.Capitulo 4/Materialismo.html' },
+        { text: 'Idealismo', link: '/.Capitulo 4/Idealismo.html' },
+        { text: 'Dualismo', link: '/.Capitulo 4/Dualismo.html' },
+        { text: 'Existencialismo', link: '/.Capitulo 4/Existencialismo.html' },
+        { text: 'Pragmatismo', link: '/.Capitulo 4/Pragmatismo.html' },
+        { text: 'Postmodernismo', link: '/.Capitulo 4/Postmodernismo.html' },
+        { text: 'Conclusión', link: '/.Capitulo 4/Conclusion.html' }
       ],
 
       'capitulo5': [
-        { text: 'Visión General', link: '/Capitulo 5/VisionGeneral.html' },
-        { text: 'Racionalismo', link: '/Capitulo 5/Racionalismo.html' },
-        { text: 'Empirismo', link: '/Capitulo 5/Empirismo.html' },
-        { text: 'Transcendentalismo', link: '/Capitulo 5/Transcendentalismo.html' },
-        { text: 'Escepticismo', link: '/Capitulo 5/Escepticismo.html' },
-        { text: 'Tipos de Conocimiento', link: '/Capitulo 5/TiposDeConocimiento.html' },
-        { text: 'Verdad', link: '/Capitulo 5/Verdad.html' },
-        { text: 'Verdad y Ciencia', link: '/Capitulo 5/VerdadYCiencia.html' },
-        { text: 'Conclusión', link: '/Capitulo 5/Conclusion.html' }
+        { text: 'Visión General', link: '/.Capitulo 5/VisionGeneral.html' },
+        { text: 'Racionalismo', link: '/.Capitulo 5/Racionalismo.html' },
+        { text: 'Empirismo', link: '/.Capitulo 5/Empirismo.html' },
+        { text: 'Transcendentalismo', link: '/.Capitulo 5/Transcendentalismo.html' },
+        { text: 'Escepticismo', link: '/.Capitulo 5/Escepticismo.html' },
+        { text: 'Tipos de Conocimiento', link: '/.Capitulo 5/TiposDeConocimiento.html' },
+        { text: 'Verdad', link: '/.Capitulo 5/Verdad.html' },
+        { text: 'Verdad y Ciencia', link: '/.Capitulo 5/VerdadYCiencia.html' },
+        { text: 'Conclusión', link: '/.Capitulo 5/Conclusion.html' }
       ],
 
       'capitulo6': [
-        { text: 'Visión General', link: '/Capitulo 6/VisionGeneral.html' },
-        { text: 'El Problema Mente-Cuerpo', link: '/Capitulo 6/ProblemaMenteCuerpo.html' },
-        { text: 'Dualismo', link: '/Capitulo 6/Dualismo.html' },
-        { text: 'Monismo: Idealismo', link: '/Capitulo 6/MonismoIdealismo.html' },
-        { text: 'Monismo: Materialismo', link: '/Capitulo 6/MonismoMaterialismo.html' },
-        { text: 'Conclusión', link: '/Capitulo 6/Conclusion.html' }
+        { text: 'Visión General', link: '/.Capitulo 6/VisionGeneral.html' },
+        { text: 'El Problema Mente-Cuerpo', link: '/.Capitulo 6/ProblemaMenteCuerpo.html' },
+        { text: 'Dualismo', link: '/.Capitulo 6/Dualismo.html' },
+        { text: 'Monismo: Idealismo', link: '/.Capitulo 6/MonismoIdealismo.html' },
+        { text: 'Monismo: Materialismo', link: '/.Capitulo 6/MonismoMaterialismo.html' },
+        { text: 'Conclusión', link: '/.Capitulo 6/Conclusion.html' }
       ],
 
       'capitulo7': [
-        { text: 'Visión General', link: '/Capitulo 7/VisionGeneral.html' },
-        { text: 'Libertad I', link: '/Capitulo 7/LibertadI.html' },
-        { text: 'Libertad II', link: '/Capitulo 7/LibertadII.html' },
-        { text: 'Libertad III', link: '/Capitulo 7/LibertadIII.html' },
-        { text: 'Libertarismo', link: '/Capitulo 7/Libertarismo.html' },
-        { text: 'Libertad Radical y Existencialismo', link: '/Capitulo 7/LibertadRadicalExistencialismo.html' },
-        { text: 'Determinismo', link: '/Capitulo 7/Determinismo.html' },
-        { text: 'Compatibilismo', link: '/Capitulo 7/Compatibilismo.html' },
-        { text: 'Conclusión', link: '/Capitulo 7/Conclusion.html' }
+        { text: 'Visión General', link: '/.Capitulo 7/VisionGeneral.html' },
+        { text: 'Libertad I', link: '/.Capitulo 7/LibertadI.html' },
+        { text: 'Libertad II', link: '/.Capitulo 7/LibertadII.html' },
+        { text: 'Libertad III', link: '/.Capitulo 7/LibertadIII.html' },
+        { text: 'Libertarismo', link: '/.Capitulo 7/Libertarismo.html' },
+        { text: 'Libertad Radical y Existencialismo', link: '/.Capitulo 7/LibertadRadicalExistencialismo.html' },
+        { text: 'Determinismo', link: '/.Capitulo 7/Determinismo.html' },
+        { text: 'Compatibilismo', link: '/.Capitulo 7/Compatibilismo.html' },
+        { text: 'Conclusión', link: '/.Capitulo 7/Conclusion.html' }
       ],
 
       'capitulo8': [
-        { text: 'Visión General', link: '/Capitulo 8/VisionGeneral.html' },
-        { text: 'Costumbres, Ley y Moralidad', link: '/Capitulo 8/CostumbresLeyMoralidad.html' },
-        { text: 'Desarrollo Moral', link: '/Capitulo 8/DesarrolloMoral.html' },
-        { text: 'Relativismo', link: '/Capitulo 8/Relativismo.html' },
-        { text: 'Postmodernismo: Relativismo Ético Normativo', link: '/Capitulo 8/PostmodernismoRelativismoEtico.html' },
-        { text: 'Teorías Éticas', link: '/Capitulo 8/TeoriasEticas.html' },
-        { text: 'Teorías Teleológicas', link: '/Capitulo 8/TeoriasTeleologicas.html' },
-        { text: 'Utilitarismo', link: '/Capitulo 8/Utilitarismo.html' },
-        { text: 'Teorías Deontológicas', link: '/Capitulo 8/TeoriasDeontologicas.html' },
-        { text: 'Teoría del Derecho Natural', link: '/Capitulo 8/TeoriaDerechoNatural.html' },
-        { text: 'Imperativo Categórico', link: '/Capitulo 8/ImperativoCategorico.html' },
-        { text: 'Justicia como Equidad', link: '/Capitulo 8/JusticiaComoEquidad.html' },
-        { text: 'Postmodernismo', link: '/Capitulo 8/Postmodernismo.html' },
-        { text: 'Conclusión', link: '/Capitulo 8/Conclusion.html' }
+        { text: 'Visión General', link: '/.Capitulo 8/VisionGeneral.html' },
+        { text: 'Costumbres, Ley y Moralidad', link: '/.Capitulo 8/CostumbresLeyMoralidad.html' },
+        { text: 'Desarrollo Moral', link: '/.Capitulo 8/DesarrolloMoral.html' },
+        { text: 'Relativismo', link: '/.Capitulo 8/Relativismo.html' },
+        { text: 'Postmodernismo: Relativismo Ético Normativo', link: '/.Capitulo 8/PostmodernismoRelativismoEtico.html' },
+        { text: 'Teorías Éticas', link: '/.Capitulo 8/TeoriasEticas.html' },
+        { text: 'Teorías Teleológicas', link: '/.Capitulo 8/TeoriasTeleologicas.html' },
+        { text: 'Utilitarismo', link: '/.Capitulo 8/Utilitarismo.html' },
+        { text: 'Teorías Deontológicas', link: '/.Capitulo 8/TeoriasDeontologicas.html' },
+        { text: 'Teoría del Derecho Natural', link: '/.Capitulo 8/TeoriaDerechoNatural.html' },
+        { text: 'Imperativo Categórico', link: '/.Capitulo 8/ImperativoCategorico.html' },
+        { text: 'Justicia como Equidad', link: '/.Capitulo 8/JusticiaComoEquidad.html' },
+        { text: 'Postmodernismo', link: '/.Capitulo 8/Postmodernismo.html' },
+        { text: 'Conclusión', link: '/.Capitulo 8/Conclusion.html' }
       ],
 
       'capitulo9': [
-        { text: 'Filosofía Social', link: '/Capitulo 9/FilosofiaSocial.html' },
-        { text: 'Liberalismo y Conservadurismo', link: '/Capitulo 9/LiberalismoYConservadurismo.html' }
+        { text: 'Filosofía Social', link: '/.Capitulo 9/FilosofiaSocial.html' },
+        { text: 'Liberalismo y Conservadurismo', link: '/.Capitulo 9/LiberalismoYConservadurismo.html' }
       ],
 
       'capitulo10': [
-        { text: 'Filosofía Política', link: '/Capitulo 10/FilosofiaPolitica.html' },
-        { text: 'Interés del Grupo vs. Interés Individual', link: '/Capitulo 10/InteresGrupoVsIndividual.html' },
-        { text: 'Desigualdad', link: '/Capitulo 10/Desigualdad.html' }
+        { text: 'Filosofía Política', link: '/.Capitulo 10/FilosofiaPolitica.html' },
+        { text: 'Interés del Grupo vs. Interés Individual', link: '/.Capitulo 10/InteresGrupoVsIndividual.html' },
+        { text: 'Desigualdad', link: '/.Capitulo 10/Desigualdad.html' }
       ],
 
         'capitulo11': [
-        { text: 'EN DESARROLLO', link: '/Capitulo 11/error.html' },
+        { text: 'EN DESARROLLO', link: '/.Capitulo 11/error.html' },
         ],
 
       'capitulo12': [
-        { text: 'Últimos Temas', link: '/Capitulo 12/UltimosTemas.html' },
-        { text: '¿Qué es la Filosofía?', link: '/Capitulo 12/QueEsLaFilosofia.html' }
+        { text: 'Últimos Temas', link: '/.Capitulo 12/UltimosTemas.html' },
+        { text: '¿Qué es la Filosofía?', link: '/.Capitulo 12/QueEsLaFilosofia.html' }
       ]    
 };
 
